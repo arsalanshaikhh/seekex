8c0dbdd2430ff8ba9ef79d2f6f9c717e1e1f6530
arsalanshaikhh <arsalan4coder@gmail.com>
2023-03-29 04:18:06 +0530

    footer updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React, { Suspense, lazy } from "react";
 import "./App.css";
+// import Navbar from "./comp/Navbar";
+
+const Navbar = lazy(() => import("./comp/Navbar"));
+const Footer = lazy(() => import("./comp/Footer"));
+const Footer2 = lazy(() => import("./comp/Footer2"));
+const Sub = lazy(() => import("./comp/Sub"));
 
 function App() {
   return (
     <div className="App">
-      <h1>aaaaaaaaaaa</h1>
+      {/* <Suspense fallback={<h3>please wait...</h3>}>
+        <Navbar />
+      </Suspense> */}
+
+      <Suspense fallback={<h3>please wait...</h3>}>
+        <Sub />
+      </Suspense>
+
+      {/* <Suspense fallback={<h3>please wait...</h3>}>
+        <Footer />
+      </Suspense>
+
+      <Suspense fallback={<h3>please wait...</h3>}>
+        <Footer2 />
+      </Suspense> */}
     </div>
   );
 }
