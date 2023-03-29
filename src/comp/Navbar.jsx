89d05653524bdbe7d650e2d55a4383fe57d9fa4c
arsalanshaikhh <arsalan4coder@gmail.com>
2023-03-30 00:52:11 +0530

    navbar updated

--- src/comp/Navbar.jsx
+++ src/comp/Navbar.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import { Box, Center, Flex, Spacer, Square, Text } from "@chakra-ui/react";
 import { BiBox } from "react-icons/bi";
 import menu from "../svg/menu.svg";
+import menu1 from "../svg/menu1.svg";
+import menu2 from "../svg/menu2.svg";
+import menu3 from "../svg/menu3.svg";
+import menu4 from "../svg/menu4.svg";
 
 const Navbar = () => {
   return (
@@ -10,20 +14,28 @@ const Navbar = () => {
       h={"60px"}
       alignItems="center"
       color="white"
-      p={"0 10%"}
+      p={"0 12%"}
       justifyContent={"space-between"}
     >
-      <Flex bg="green.500">
+      <Flex>
         <img src={menu} />
       </Flex>
-      <Box bg="blue.500">
-        <Text>Logo Here</Text>
+      <Box>
+        <Text
+          color={" #1C1C1C"}
+          fontSize={"16px"}
+          fontWeight="600"
+          lineHeight={"21px"}
+          fontFamily={"Playfair Display"}
+        >
+          Logo Here
+        </Text>
       </Box>
-      <Flex w="15%" bg="tomato" justifyContent={"space-evenly"}>
-        <img src={menu} />
-        <img src={menu} />
-        <img src={menu} />
-        <img src={menu} />
+      <Flex w="17%" justifyContent={"space-evenly"}>
+        <img src={menu1} />
+        <img src={menu2} />
+        <img src={menu3} />
+        <img src={menu4} />
       </Flex>
     </Flex>
   );
